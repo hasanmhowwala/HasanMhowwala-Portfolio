19bbb6310c372ca24c9ca59ec08e0679d8942bfa
hasanmhowwala <143671213+hasanmhowwala@users.noreply.github.com>
2024-11-12 03:34:30 -0800

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Form } from "./components";
 
 const App = () => {
   return (
@@ -14,9 +14,10 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />  
+        {/* <Feedbacks />   */}
         <div className='relative z-0'>
           <Contact />
+          <Form />
           <StarsCanvas />
         </div>
       </div>
